fd5aeaea8a981b748cffbb7b04a75ae007ea2935
ericbui2@gmail.com <ebui91@Erics-MacBook-Pro.local>
2017-10-11 09:37:27 -0500

    the notorious FIZZBUZZ

---
+++ fizzbuzz.js
@@ -0,0 +1,26 @@
+// Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value 'Fizz' instead
+// If the value is a multiple of 5: use the value 'Buzz' instead
+// If the value is a multiple of 3 & 5: use the value 'FizzBuzz' instead
+
+function fizzbuzz(n){
+  var answer=[];
+
+  for(var i=1;i<=n;i++){
+    if(i%3===0&&i%5!==0){
+      answer.push("Fizz");
+    }else if(i%3!==0&&i%5===0){
+      answer.push("Buzz");
+    }else if(i%3===0&&i%5===0){
+      answer.push("FizzBuzz");
+    }else{
+      answer.push(i);
+    }
+  }
+  return answer;
+}
+
+fizzbuzz(15);
